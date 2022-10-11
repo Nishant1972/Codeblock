fb024414f5c91b76685ecc7d8d7240a232e8cc78
Nishant1972 <56684286+Nishant1972@users.noreply.github.com>
2022-10-11 17:36:57 +0530

    Update Q11.cpp

--- Q11.cpp
+++ Q11.cpp
@@ -1,12 +1,14 @@
 class Solution {
-    int maxArea(int[] heights) {
-        int ans = 0, i = 0, j = heights.size() - 1;
+    int maxArea(int[] h) {
+        int ans = 0, i = 0, j = h.size() - 1;
         while(i < j){
-            int width = j - i, height = min(heights[i], heights[j]);
-            ans = max(ans, width * height);
+            int width = j - i, height = min(h[i], h[j]);
+            ans = max(ans, width * h);
             
-            if(heights[i] < heights[j]) i += 1;
+            if(h[i] < h[j]) i += 1;
             else j += -1;
-        } return ans;
+        }
+        
+        return ans;
     }
 }
